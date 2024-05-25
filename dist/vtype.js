fb748f4bae9c646fe70c9199d03ab3017e148d54
Ali <aliarain2246@gmail.com>
2024-05-25 05:13:16 -0700

    variable types

---
+++ dist/vtype.js
@@ -0,0 +1,13 @@
+"use strict";
+// reserved space   i am a box     put the thing that suit me      thing that suit this type of variable
+//    ^                  ^                 ^                              ^
+// keyword           variable      type of variable                     value
+// let              userName       : string                       = "ALI ARAIN";
+let personName = "ALI ARAIN";
+let age = 25;
+let isStudent = false;
+let randomValue = "http://www.google.com";
+console.log(personName);
+console.log(age);
+console.log(isStudent);
+console.log(randomValue);
