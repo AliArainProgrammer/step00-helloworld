a119ead4fb3b62f338b016f447995a2ebd4a36ca
Ali <aliarain2246@gmail.com>
2024-05-25 04:25:51 -0700

    arthimetic operators

---
+++ src/arthemetic-operator.ts
@@ -0,0 +1,25 @@
+// arthemetic operators are (+,-,*,/,%,**,++,--). Thet are use for simple calculation
+
+let Myapplicationfunding: number = 0;
+
+//Add Funds
+
+const google: number = 200000;
+const investor: number = 500000;
+const facebook: number = 1000000;
+const alonmusk: number = 10000000;
+
+Myapplicationfunding = google + investor + facebook + alonmusk;   //11,700,000
+
+console.log('total funds you get =', Myapplicationfunding);
+
+let fundinginvestmentincompany: number = 0;
+
+//Start investment
+
+const cloudspace: number = 700000;
+const marketing: number = 1000000;
+const employes: number =  5000000;
+const metaAi: number =  5000000;
+
+
