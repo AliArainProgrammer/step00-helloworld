9e7e715c45278857966cd99580dfb8a65b6a5cf0
Ali <aliarain2246@gmail.com>
2024-05-28 10:46:48 -0700

    String Comparision In Operators

--- dist/operator-string-comparision.js
+++ dist/operator-string-comparision.js
@@ -1,10 +1,12 @@
 "use strict";
 // STRING COMPARISION 
 //Operators are also use to compare strings 
+// string number comparision
 let mynamefirstletter = 'A';
 let mynamesecondletter = 'L';
 let result = mynamefirstletter < mynamesecondletter;
 console.log('letter output', result);
+// string letter comparision
 let num = '20';
 let num1 = '80';
 let output = num > num1;
