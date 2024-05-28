94fa362c88f7cc68d533aa6bf835fc510f2102ce
Ali <aliarain2246@gmail.com>
2024-05-28 10:56:09 -0700

    Add Subtract Multiply Strings Using Operators

---
+++ src/string-add-sub-multi.ts
@@ -0,0 +1,25 @@
+//OPERATORS ARE ALSO USE FOR JOINING OR COMBINING TWO WORDS SENTENCES OR WHAT ENER YOU WANT.
+// + operator ko string par jab use kartay hn  to us ko concatenation boltay hn
+let myname: string = 'ALI'
+let myName: string = 'IMTIAZ'
+console.log(myname +" "+ myName); //concatenation
+
+
+let meraname: string = 'ALI'
+let meraName: string = 'IMTIAZ'
+let MeraName: string = 'ARAIN'
+
+console.log(myname +" "+ myName +" "+ MeraName); //concatenation
+
+let dostkaname: string = 'RIYAN'
+dostkaname +=" " + 'KHAN'
+
+console.log('dost ka name',dostkaname); //concatenation
+
+//repeating an string with the help of adding number to string
+let a: number = 5 + 5;
+let b: string = "5" + 5;
+let c: string = "Assalam-u-Alikum" + 5;
+
+console.log(a,b,c);
+ 
