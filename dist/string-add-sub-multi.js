d8f5d080d77ee18c724af00b0980713d57c2c409
Ali <aliarain2246@gmail.com>
2024-05-28 10:50:02 -0700

    Add Subtract Multiply Strings Using Operators

---
+++ dist/string-add-sub-multi.js
@@ -0,0 +1,18 @@
+"use strict";
+//OPERATORS ARE ALSO USE FOR JOINING OR COMBINING TWO WORDS SENTENCES OR WHAT ENER YOU WANT.
+// + operator ko string par jab use kartay hn  to us ko concatenation boltay hn
+let myname = 'ALI';
+let myName = 'IMTIAZ';
+console.log(myname + " " + myName); //concatenation
+let meraname = 'ALI';
+let meraName = 'IMTIAZ';
+let MeraName = 'ARAIN';
+console.log(myname + " " + myName + " " + MeraName); //concatenation
+let dostkaname = 'RIYAN';
+dostkaname += " " + 'KHAN';
+console.log('dost ka name', dostkaname); //concatenation
+//repeating an string with the help of adding number to string
+let a = 5 + 5;
+let b = "5" + 5;
+let c = "Assalam-u-Alikum" + 5;
+console.log(a, b, c);
