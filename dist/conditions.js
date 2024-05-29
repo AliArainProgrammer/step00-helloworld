842909498bc3900cebd5539b9054b446a47c2243
Ali <aliarain2246@gmail.com>
2024-05-29 10:38:34 -0700

    Conditions in typescript

---
+++ dist/conditions.js
@@ -0,0 +1,23 @@
+"use strict";
+// they are used to perform actions according to different situations.
+Object.defineProperty(exports, "__esModule", { value: true });
+let mysavings = 12000;
+if (mysavings >= 11000) {
+    console.log("you can do what you want");
+}
+else {
+    console.log("you can not do what you want sorry");
+}
+let mericyclekokiaboloun = "mushi";
+if (mericyclekokiaboloun === "maaz") {
+    console.log("meri bugati");
+}
+else if (mericyclekokiaboloun === "zuhaib") {
+    console.log("meri royls royls");
+}
+else if (mericyclekokiaboloun === "asad") {
+    console.log("meri farari");
+}
+else {
+    console.log("chup betha rah yar");
+}
---
+++ src/conditions.ts
@@ -0,0 +1,38 @@
+// they are used to perform actions according to different situations.
+
+import { log } from "console";
+
+let mysavings: number = 12000;
+
+if (mysavings >= 11000) {
+    console.log("you can do what you want");
+    
+}
+
+else {
+        console.log("you can not do what you want sorry");
+
+}
+
+let mericyclekokiaboloun: string = "maaz";
+
+if (mericyclekokiaboloun === "maaz") {
+    console.log("meri bugati");
+
+}
+
+else if (mericyclekokiaboloun === "zuhaib") {
+    console.log("meri royls royls");
+    
+}
+
+else if (mericyclekokiaboloun === "asad") {
+    console.log("meri farari");
+    
+}
+
+else{
+    console.log("chup betha rah yar");
+    
+}
+
